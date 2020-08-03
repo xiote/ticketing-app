2f086294c5db264c9f5ba37cb00777a2b507fa89
xiote <xiote@nate.com>
2020-08-04 07:24:21 +0900

    new file:   main.go

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/tebeka/selenium"
+	"github.com/xiote/interparkcontroller"
+)
+
+func main() {
+	Example()
+}
+
+func Example() {
+
+	// Start a Selenium WebDriver server instance (if one is not already
+	// running).
+	selenium.SetDebug(true)
+
+	// Connect to the WebDriver instance running locally.
+	caps := selenium.Capabilities{"browserName": "chrome"}
+	wd, err := selenium.NewRemote(caps, "http://localhost:4444/wd/hub")
+	if err != nil {
+		panic(err)
+	}
+	defer wd.Quit()
+
+	c := interparkcontroller.NewController2(wd, interparkcontroller.LoginInfo{"xiote12", "gkswlsdn78#"})
+	if err := c.Login(); err != nil {
+		panic(err)
+	}
+
+}
